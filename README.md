f576ca0a9992710fad84c9f021f2ec1fc0f4463f
Jenek <Jenek619@gmail.com>
2023-09-02 11:42:09 +0300

    Commit2

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/mrbd1g2r1cnn24u9?svg=true)](https://ci.appveyor.com/project/Jenek619/patterns2)
---
+++ src/test/java/ru/netology/UserTest.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.DataGenerator.Registration.getRegisteredUser;
+
+public class UserTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+
+    }
+}
